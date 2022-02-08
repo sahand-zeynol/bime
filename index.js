ed71b4641dc2427374e8f13d8ef1bc41f9bacd6c
Sahand Zeynol <sahand.zeynol@gmail.com>
2022-02-09 02:08:00 +0330

    set fromDate and toDate dynamicaly

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const puppeteer = require("puppeteer");
 // const ExcelService = require("./xlsx.service");
 const title = [];
 const ids = [];
+const fromDate = new Date(new Date().setDate(new Date().getDate() - 2))
+  .toISOString()
+  .split("T")[0];
+const toDate = new Date(Date.now()).toISOString().split("T")[0];
 const loginUrl = "http://carbill.ir/partner/login";
 const panelUrl = "http://carbill.ir/partner/insurance";
-const filteredUrl =
-  "http://carbill.ir/partner/insurance?mobile=&buyer_id=&national_code=&status%5B%5D=ORDERED&status%5B%5D=ASSIGN_TO_PARTNER&status%5B%5D=INCOMPLETE&status%5B%5D=CONFLICT_RESOLVED&status%5B%5D=PRICE_CONFLICT&start_date=2022-02-02&end_date=2022-02-05&pay_status=PAID&plate-ir=&plate-p3=&plate-p2=01&plate-p1=";
-const fromDate = new Date(2022, 02, 02);
-const toDate = new Date(2022, 02, 04);
+const filteredUrl = `http://carbill.ir/partner/insurance?mobile=&buyer_id=&national_code=&status%5B%5D=ORDERED&status%5B%5D=ASSIGN_TO_PARTNER&status%5B%5D=INCOMPLETE&status%5B%5D=CONFLICT_RESOLVED&status%5B%5D=PRICE_CONFLICT&start_date=${fromDate}&end_date=${toDate}&pay_status=PAID&plate-ir=&plate-p3=&plate-p2=01&plate-p1=`;
 const username = "hor4451";
 const password = "09123063070";
 const excel = [
