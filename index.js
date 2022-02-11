98bca350bccedde35250a920f81dc40681f0197a
Sahand Zeynol <sahand.zeynol@gmail.com>
2022-02-11 19:39:05 +0330

    export data with excel

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
 const puppeteer = require("puppeteer");
-// const prompt = require("prompt-sync")();
-// const ExcelService = require("./xlsx.service");
-const title = [];
-const ids = [];
+const xl = require("excel4node");
 const fromDate = new Date(new Date().setDate(new Date().getDate() - 2))
   .toISOString()
   .split("T")[0];
 const toDate = new Date(Date.now()).toISOString().split("T")[0];
 const loginUrl = "http://carbill.ir/partner/login";
-const panelUrl = "http://carbill.ir/partner/insurance";
 const filteredUrl = `http://carbill.ir/partner/insurance?mobile=&buyer_id=&national_code=&status%5B%5D=ORDERED&status%5B%5D=ASSIGN_TO_PARTNER&status%5B%5D=INCOMPLETE&status%5B%5D=CONFLICT_RESOLVED&status%5B%5D=PRICE_CONFLICT&start_date=${fromDate}&end_date=${toDate}&pay_status=PAID&plate-ir=&plate-p3=&plate-p2=01&plate-p1=`;
 const username = "hor4451";
 const password = "09123063070";
-const excel = [
-  {
-    date: 1,
-    invoiceId: 1,
-    amount: 1,
-  },
-];
-const exportColumns = ["date", "invoiceId", "amount"];
+const wb = new xl.Workbook();
 
 async function run() {
   const browser = await puppeteer.launch({
@@ -44,6 +33,9 @@ async function run() {
   });
   console.log(rows);
   for (let row of rows) {
+    const ws = wb.addWorksheet(row);
+    ws.cell(1, 1).string("name");
+    ws.cell(1, 2).string("detail");
     await page.click(`#${row} > td:nth-child(16) > a:nth-child(1)`);
     await page.waitForTimeout(3000);
     await page.waitForSelector(".modal.show table tr td");
@@ -53,6 +45,13 @@ async function run() {
       );
       return tds.map((td) => td.innerText);
     });
+    for (var i = 0, len = data.length; i < len; i++) {
+      if (i % 2 === 0) {
+        ws.cell(i / 2 + 2, 1).string(data[i]);
+      } else {
+        ws.cell((i + 1) / 2 + 1, 2).string(data[i]);
+      }
+    }
     console.log(data);
     await page.waitForSelector(".modal.show button.close");
     await page.click(`.modal.show button.close`);
@@ -60,5 +59,6 @@ async function run() {
   }
 
   //   browser.close();
+  wb.write("Excel.xlsx");
 }
 run();
