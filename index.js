2d306aff307ca8820d677826ea73f9c0c6857e96
sahand-zeynol <sahand_zeynol@yahoo.com>
2023-11-05 13:21:22 +0330

    remove credentials

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const fromDate = new Date(new Date().setDate(new Date().getDate() - 2))
 const toDate = new Date(Date.now()).toISOString().split("T")[0];
 const loginUrl = "http://carbill.ir/partner/login";
 const filteredUrl = `http://carbill.ir/partner/insurance?mobile=&buyer_id=&national_code=&status%5B%5D=ORDERED&status%5B%5D=ASSIGN_TO_PARTNER&status%5B%5D=INCOMPLETE&status%5B%5D=CONFLICT_RESOLVED&status%5B%5D=PRICE_CONFLICT&start_date=${fromDate}&end_date=${toDate}&pay_status=PAID&plate-ir=&plate-p3=&plate-p2=01&plate-p1=`;
-const username = "hor4451";
-const password = "09123063070";
+const username = "user";
+const password = "password";
 const wb = new xl.Workbook();
 
 async function run() {
